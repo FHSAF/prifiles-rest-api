43171d28bfa0f39fe599b9f12f1dc40a50a224bb
myGit@git.com <myGit@git.com>
2020-02-08 21:14:39 +0600

    adding superuser on django admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -22,7 +22,7 @@ class UserProfileManager(BaseUserManager):
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with"""
         user = self.create_user(email, name, password)
-        user.is_superuesr = True
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
