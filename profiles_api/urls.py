236c9f8c315b24c400f34900ffb0c0d05ed52c05
myGit@git.com <myGit@git.com>
2020-02-09 15:54:49 +0600

    added login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
-from .views import HelloApiView, HellViewSet
+from .views import HelloApiView, HellViewSet, UserProfileViewSet, UserLoginApiView
 
 router = DefaultRouter()
 router.register('viewset', HellViewSet, basename='viewset')
+router.register('profile', UserProfileViewSet)
 
 urlpatterns = [
     path('APIView', HelloApiView.as_view()),
+    path('login/', UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
