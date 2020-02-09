05abfe28f05ae7fff0629ad1cbb53c7a3430f51e
myGit@git.com <myGit@git.com>
2020-02-09 13:58:30 +0600

    tested HelloAPIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API vIEW"""
+
+    def get(self, request, fromat=None):
+        """returns a list of APIView freatues"""
+        an_apiview = [
+            'Use HTTP methods as function (get, post, pathc, put, delete',
+            'Is mapped manually to URLs',
+        ]
+        return Response({"message": "Hello", "an_piview": an_apiview})
