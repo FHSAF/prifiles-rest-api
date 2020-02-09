7b8a3bb9af5c17e7edbb5315d3302693e3a8e246
myGit@git.com <myGit@git.com>
2020-02-09 14:55:09 +0600

    add viewset into the profiles_api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import HelloApiView
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from .views import HelloApiView, HellViewSet
+
+router = DefaultRouter()
+router.register('viewset', HellViewSet, basename='viewset')
 
 urlpatterns = [
-    path('', HelloApiView.as_view()),
+    path('APIView', HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
